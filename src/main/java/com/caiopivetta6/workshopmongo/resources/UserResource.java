0782a7cd2b9e5d61d28e7c9c664168fd05993f31
Caio Pivetta <caiopivetta6gmail.com>
2022-12-26 17:58:36 +0100

    Operation Instantiation database

--- src/main/java/com/caiopivetta6/workshopmongo/resources/UserResource.java
+++ src/main/java/com/caiopivetta6/workshopmongo/resources/UserResource.java
@@ -2,6 +2,7 @@ package com.caiopivetta6.workshopmongo.resources;
 
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.caiopivetta6.workshopmongo.domain.User;
+import com.caiopivetta6.workshopmongo.dto.UserDTO;
 import com.caiopivetta6.workshopmongo.services.UserService;
 
 @RestController
@@ -20,9 +22,10 @@ public class UserResource {
 	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<List<User>>  findAll(){
+	public ResponseEntity<List<UserDTO>>  findAll(){
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 	
 	
